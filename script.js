f911fb698909d63785603448350a0338ad6f1f06
shaheenahmd <shaheenahmdk@gmail.com>
2022-11-15 22:58:27 +0530

    third

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menu.addEventListener("click", function(){
+    navlist.classList.toggle("active");
+})
+window.onscroll =()=>{
+    navlist.classList.remove("active")
+};
+
+
+
 // var typed = new Typed(".input",{
 //     strings:["Fullstack Developer", "Designer", "Freelancer"],
 //     typeSpeed:120,
